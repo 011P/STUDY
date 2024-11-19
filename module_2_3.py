3c26a7e10133039dbd9c38b8d515ff9ad96f5f5a
011P <133758351+011P@users.noreply.github.com>
2024-11-20 01:18:37 +0500

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,16 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+x = 0
+
+while x < len(my_list):
+    number = my_list[x]
+    x = x + 1
+    if number == 0:
+        continue
+    elif number < 0:
+        print('число отрицательно:', number)
+        break
+    elif x == len(my_list):
+        print(number)
+        print('выход за границу')
+    else:
+        print(number)
